8d473ea6389c3f5d5c6a9510e81bc78b543dcd2b
Osher Hajaj <osherx2@gmail.com>
2024-12-20 08:14:28 +0200

    update ValidOrderAmount validator max value

--- src/main/java/com/ecommerce/common/order/DTO/ProductDTO.java
+++ src/main/java/com/ecommerce/common/order/DTO/ProductDTO.java
@@ -7,7 +7,7 @@ import com.ecommerce.common.order.validation.ValidOrderAmount;
 
 public record ProductDTO(
         @NotNull UUID productId,
-        @ValidOrderAmount(min = 1, max = 50, message = "Order amount must be between 1 and 50")
+        @ValidOrderAmount(min = 1, max = 30, message = "Order amount must be between 1 and 30")
         @NotNull @Positive int quantity)
 {
 }
